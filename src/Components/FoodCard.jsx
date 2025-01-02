d969651d8c371ee1b885ee8caedc6545da133829
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-02 23:44:16 +0600

    cart item with email id send to the db and use sweetalert a nd created useAxiosSecure custom hooks

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -1,17 +1,41 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Swal from "sweetalert2";
+import axios from "axios";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
+const FoodCard = ({ item }) => {
+  const { image, price, name, recipe, _id } = item;
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const axiosSecure = useAxiosSecure()
 
-const FoodCard = ({item}) => {
-	const {image, price, name,recipe } = item;
-  const {user} = useAuth()
-  const navigate = useNavigate()
+  const handleAddToCart = (food) => {
+    if (user && user.email) {
+      const cartItem = {
+        menuId: _id,
+        email: user.email,
+        name,
+        image,
+        price,
+      };
+      axiosSecure.post('/carts', cartItem)
+      .then(res => {
+        console.log(res.data)
+        if(res.data.insertedId){
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `${name} added to  cart`,
+                showConfirmButton: false,
+                timer: 1500
+              });
 
-  const handleAddToCart = food => {
-    if(user && user.email){
-
-    }else{
+        }
+        
+      })
+    } else {
       Swal.fire({
         title: "You are not logged in",
         text: "Please Log in to add to cart!",
@@ -19,33 +43,36 @@ const FoodCard = ({item}) => {
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, Log in!"
+        confirmButtonText: "Yes, Log in!",
       }).then((result) => {
         if (result.isConfirmed) {
-          navigate('/login')
+          navigate("/login", { state: { from: location } });
         }
       });
-
     }
-  }
-	return (
-		<div className="card bg-base-100 w-96 shadow-xl">
-			
-  <figure>
-    <img
-      src={image}
-      alt="Shoes" />
-  </figure>
-  <p className="text-white absolute bg-slate-500 mt-4 ml-80 px-2 font-semibold">{price}</p>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p>{recipe}</p>
-    <div className="card-actions justify-center">
-      <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4   mt-4 border-orange-400 bg-orange-100">ADD TO CART</button>
+  };
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <p className="text-white absolute bg-slate-500 mt-4 ml-80 px-2 font-semibold">
+        {price}
+      </p>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-center">
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="btn btn-outline border-0 border-b-4   mt-4 border-orange-400 bg-orange-100"
+          >
+            ADD TO CART
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-	);
+  );
 };
 
 export default FoodCard;
