6ea20e76778d8965aedf119f33bd0e86d783ba9e
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-03 11:25:22 +0600

    reduce imlemente for adding total items price and show items in table in cart compo

--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -1,8 +1,75 @@
+import useCart from "../../hooks/useCart";
 
 const Cart = () => {
+    const [cart] = useCart()
+    const totalPrice = cart.reduce((previousValue, currentValue) => previousValue + currentValue.price ,0)
     return (
         <div>
-            <h2 className="text-6xl">My Cart</h2>
+        <div className="
+        flex justify-evenly mt-4 bg-orange-400 text-white p-2">
+        <h2 className="text-4xl">Items :{cart.length} </h2>
+        <h2 className="text-4xl">Total Price :{totalPrice} </h2>
+        <button className="btn btn-error">Pay</button>
+        </div>
+
+        {/* table content */}
+        <div>
+        <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          No
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            cart.map((item, idx )=> 
+                <tr key={item._id}>
+                <th>
+                {idx +1}
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                     
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            )
+        }
+      {/* row 1 */}
+
+     
+    </tbody>
+    
+  </table>
+</div>
+        </div>
             
         </div>
     );
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,30 +1,73 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import {
+  FaAd,
+  FaCalendar,
+  FaHome,
+  FaList,
+  FaSearch,
+  FaShoppingCart,
+} from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div className="flex gap-4">
+  return (
+    <div className="flex gap-4">
+      {/* sidebar content */}
+      <div className="w-64 min-h-screen bg-orange-400">
+        <ul className="menu p-4 text-white">
+          <li>
+            <NavLink to="/dashboard/home">
+              {" "}
+              <FaHome></FaHome>User HOme
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              {" "}
+              <FaShoppingCart></FaShoppingCart> My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              {" "}
+              <FaCalendar></FaCalendar> Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/review">
+              {" "}
+              <FaAd></FaAd> Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+              {" "}
+              <FaList></FaList> My Bookings
+            </NavLink>
+          </li>
 
-            {/* sidebar content */}
-            <div className="w-64 min-h-screen bg-orange-400">
-                <ul className="menu p-4 text-white">
+          
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              {" "}
+              <faHome></faHome> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              {" "}
+              <FaSearch></FaSearch> Menu
+            </NavLink>
+          </li>
+        </ul>
+      </div>
 
-                    <li><NavLink to='/dashboard/home'> <FaHome></FaHome>User HOme</NavLink></li>
-                    <li><NavLink to='/dashboard/cart'> <FaShoppingCart></FaShoppingCart> My Cart</NavLink></li>
-                    <li><NavLink to='/dashboard/reservation'> <FaCalendar></FaCalendar> Reservation</NavLink></li>
-                    <li><NavLink to='/dashboard/review'> <FaAd></FaAd> Review</NavLink></li>
-                    <li><NavLink to='/dashboard/bookings'> <FaList></FaList> My Bookings</NavLink></li>
-                    {/* <li><NavLink to=''>My Cart</NavLink></li> */}
-                </ul>
-            </div>
-
-            {/* dashboard content */}
-            <div className="flex-1">
-                <Outlet></Outlet>
-            </div>
-            
-        </div>
-    );
+      {/* dashboard content */}
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
