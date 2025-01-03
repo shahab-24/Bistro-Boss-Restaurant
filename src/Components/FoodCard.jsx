5aaa1037e94e01ee393b1de381fda6f0634918bd
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-03 15:18:24 +0600

    user wise cart item list shown and delete functioin added with refetch

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -1,7 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Swal from "sweetalert2";
-import axios from "axios";
+
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import useCart from "../hooks/useCart";
 
--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -1,12 +1,50 @@
+import { FaTrash } from "react-icons/fa";
 import useCart from "../../hooks/useCart";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const Cart = () => {
-    const [cart] = useCart()
+    const [cart, refetch] = useCart()
+    const axiosSecure = useAxiosSecure()
     const totalPrice = cart.reduce((previousValue, currentValue) => previousValue + currentValue.price ,0)
+
+    const handleDelete=(id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/carts/${id}`)
+                .then(res => {
+                    if(res.data.deletedCount > 0){
+                        refetch()
+                              Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                              })
+
+                    }
+                })
+                
+            //   Swal.fire({
+            //     title: "Deleted!",
+            //     text: "Your file has been deleted.",
+            //     icon: "success"
+            //   });
+            }
+          });
+    }
     return (
         <div>
         <div className="
-        flex justify-evenly mt-4 bg-orange-400 text-white p-2">
+        flex justify-evenly mt-4 bg-orange-400 text-white p-2 my-6">
         <h2 className="text-4xl">Items :{cart.length} </h2>
         <h2 className="text-4xl">Total Price :{totalPrice} </h2>
         <button className="btn btn-error">Pay</button>
@@ -18,13 +56,13 @@ const Cart = () => {
   <table className="table">
     {/* head */}
     <thead>
-      <tr>
+      <tr className="text-2xl font-semibold">
         <th>
           No
         </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Image</th>
+        <th>Item Name</th>
+        <th>Price</th>
         <th>Action</th>
       </tr>
     </thead>
@@ -45,24 +83,22 @@ const Cart = () => {
                       </div>
                     </div>
                     <div>
-                      <div className="font-bold">Hart Hagerty</div>
+                
                      
                     </div>
                   </div>
                 </td>
                 <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                 {item.name}
                 </td>
-                <td>Purple</td>
+                <td>$ {item.price}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
+                  <button onClick={()=>handleDelete(item._id)} className="btn btn-ghost btn-lg text-red-600"><FaTrash></FaTrash></button>
                 </th>
               </tr>
             )
         }
-      {/* row 1 */}
+      
 
      
     </tbody>
