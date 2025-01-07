d7fdf5838ab072676f0995c1d1b42e8a37e601ce
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-08 00:54:15 +0600

    verify admin jwt middleweare created and adminroute created also useAdmin implementd to load data by tanstack query

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -10,12 +10,13 @@ import {
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../../hooks/useCart";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
 
-    const [cart]= useCart()
+    const {cart}= useCart()
 
-    const isAdmin = true;
+    const [isAdmin] = useAdmin()
   return (
     <div className="flex gap-4">
       {/* sidebar content */}
---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+
+const useAdmin = () => {
+    const axiosSecure = useAxiosSecure()
+    const {user} = useAuth()
+    const {data: isAdmin, isPending: isAdminLoading} = useQuery({
+        queryKey: [user?.role, "admin"],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user.email}`)
+            console.log("useAdmin",res.data)
+            return res?.data?.admin
+            
+        }
+    })
+    return [isAdmin, isAdminLoading]
+    
+};
+
+export default useAdmin;
