b59d1d7e87613eed3ecca1c357705ffaf8d92278
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-02-03 17:45:45 +0600

    add items implemented with image hosting to imggbb and imageurl send to the backend

--- src/Components/AddItems.jsx
+++ src/Components/AddItems.jsx
@@ -3,22 +3,51 @@ import SectionTitle from "./SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 
+import Swal from "sweetalert2";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-  const axiosPublic = useAxiosPublic()
+  const axiosSecure = useAxiosSecure();
+  const axiosPublic = useAxiosPublic();
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const onSubmit = async (data) => {
-        console.log(data)
-        const imageFile = {image: data.image[0]}
-        const res = await axiosPublic.post(image_hosting_api, imageFile, {
-                headers: {
-                        'content-type': 'multipart/form-data'
-                }
-        })
-    console.log(res.data);
+//     console.log(data);
+
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+//     console.log(res.data);
+    if (res.data.success) {
+
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.insertedId) {
+        reset()
+        Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your Menu Item has been saved",
+                showConfirmButton: false,
+                timer: 1500
+              });
+}
+    }
+   
   };
   return (
     <div className="mt-10">
