182360a8e88bb3348319d33ec40116e7ef7075b9
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-02-04 18:50:08 +0600

    use tanstack query instead of fetch

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,19 +1,31 @@
-import { useEffect, useState } from "react"
+import { useQuery } from "@tanstack/react-query"
+// import { useEffect, useState } from "react"
+import useAxiosPublic from "./useAxiosPublic"
 
 const useMenu = () => {
-	const [menu, setMenu] = useState([])
-	const [loading, setLoading] = useState(true)
+        const axiosPublic = useAxiosPublic()
 
-	useEffect(() => {
-		fetch('http://localhost:3000/menu')
-		.then(res => res.json())
-		.then(data =>{
+        const {data: menu = [], isPending:loading} = useQuery({
+                queryKey: ['menu'],
+                queryFn: async () => {
+                        const res = await axiosPublic.get('/menu')
+                        return res.data;
+                }
+
+        })
+	// const [menu, setMenu] = useState([])
+	// const [loading, setLoading] = useState(true)
+
+	// useEffect(() => {
+	// 	fetch('http://localhost:3000/menu')
+	// 	.then(res => res.json())
+	// 	.then(data =>{
 			
-			setMenu(data)
-			setLoading(false)
-		} )
+	// 		setMenu(data)
+	// 		setLoading(false)
+	// 	} )
 		
-	},[])
+	// },[])
 	return [menu, loading]
 
 }
